458d5890d524b555614000b05bcfca08be853fdc
Eduardo Sampaio <edupcsampaio@gmail.com>
2025-05-21 16:47:01 -0300

    add create product form validation

--- src/pages/edit-products/index.tsx
+++ src/pages/edit-products/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Plus } from 'lucide-react'
 import { Product } from './product'
 import { Header } from '@/components/header'
@@ -11,26 +12,46 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from '@/components/ui/drawer'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 
 import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
 
 const createProductSchema = z.object({
   image: z.instanceof(File).refine(file => file.type.startsWith('image/'), {
     message: 'O arquivo deve ser uma imagem',
   }),
   name: z.string().trim().nonempty({ message: 'O campo é obrigatório' }),
-  price: z
-    .string()
-    .transform(val => val.replace(/\s/g, '').replace(',', '.'))
-    .refine(val => !Number.isNaN(Number(val)), { message: 'Preço inválido' })
-    .transform(Number)
-    .refine(val => val > 0, { message: 'O preço deve ser maior que zero' }),
+  price: z.coerce
+    .number()
+    .positive({ message: 'O preço deve ser maior que zero' }),
 })
 
+type FormData = z.infer<typeof createProductSchema>
+
 export function EditProducts() {
+  const [drawerOpen, setDrawerOpen] = useState(false)
+
+  const form = useForm<FormData>({
+    resolver: zodResolver(createProductSchema),
+  })
+
+  function onCreateProduct(data: FormData) {
+    console.log(data)
+    form.reset()
+    setDrawerOpen(false)
+  }
+
   return (
     <div className="px-4 w-full min-h-screen bg-zinc-100 flex flex-col">
       <Header pageTitle="Produtos" />
@@ -40,7 +61,7 @@ export function EditProducts() {
           Cadastre, edite ou apague um produto.
         </p>
 
-        <Drawer>
+        <Drawer open={drawerOpen} onOpenChange={setDrawerOpen}>
           <DrawerTrigger asChild>
             <button
               type="button"
@@ -60,37 +81,85 @@ export function EditProducts() {
               <DrawerTitle>Cadastrar novo produto</DrawerTitle>
             </DrawerHeader>
 
-            <form className="w-full space-y-3 mb-4 p-4">
-              <div className="grid w-full items-center gap-1.5">
-                <Label htmlFor="image">Selecione uma foto</Label>
-                <Input className="w-full" id="image" type="file" />
-              </div>
-              <div className="grid w-full items-center gap-1.5">
-                <Label htmlFor="name">Nome</Label>
-                <Input
-                  className="w-full"
-                  id="name"
-                  placeholder="Digite o nome do produto"
-                />
-              </div>
-              <div className="grid w-full items-center gap-1.5">
-                <Label htmlFor="price">Preço</Label>
-                <Input
-                  id="price"
-                  className="w-full"
-                  placeholder="Digite o valor do produto"
-                />
-              </div>
-            </form>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onCreateProduct)}
+                className="space-y-8"
+              >
+                <div className="w-full space-y-3 p-4">
+                  <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Nome</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Digite o nome do produto"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
 
-            <DrawerFooter>
-              <Button>Cadastrar</Button>
-              <DrawerClose>
-                <Button className="w-full" variant="outline">
-                  Cancelar
-                </Button>
-              </DrawerClose>
-            </DrawerFooter>
+                  <FormField
+                    control={form.control}
+                    name="price"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Preço</FormLabel>
+                        <FormControl>
+                          <Input
+                            id="price"
+                            type="number"
+                            className="w-full"
+                            placeholder="Digite o valor do produto"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="image"
+                    render={({ field: { value, onChange, ...fieldProps } }) => (
+                      <FormItem>
+                        <FormLabel htmlFor="image">Selecionar imagem</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...fieldProps}
+                            placeholder="Picture"
+                            type="file"
+                            accept="image/*"
+                            onChange={e => {
+                              const file = e.target.files?.[0]
+                              if (file) {
+                                onChange(file)
+                              }
+                            }}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                <DrawerFooter>
+                  <Button type="submit">Cadastrar</Button>
+                  <DrawerClose asChild>
+                    <Button className="w-full" variant="outline">
+                      Cancelar
+                    </Button>
+                  </DrawerClose>
+                </DrawerFooter>
+              </form>
+            </Form>
           </DrawerContent>
         </Drawer>
 
